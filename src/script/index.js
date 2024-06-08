eb9aad97f1a4114248f66939fcab6c35afe1defc
Hunor Czaka <czhunor@gmail.com>
2024-06-08 20:18:21 +0200

    more changes, but it looks promising

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,3 @@
-// tiling.js
-
 // Define Wrapper Classes for the External APIs
 // In case something changes in the official Plasma/KWin APIs,
 // we have to adapt only this section of the code. The main logic can remain unchanged if there are
@@ -96,14 +94,16 @@ class TilingManager {
     }
 
     registerWindow(window) {
-        this.logging.printMessage("New Window registration started...");
+        this.logging.printMessage(
+            "New Window registration started for " + window.resourceName
+        );
 
         // Check if the Window is relevant for us
         if (this.isWindowRelevant(window)) {
             // Add the Window to the list
             this.registeredWindows.push(window);
             this.logging.printMessage(
-                "New Window added to the Tiling Manager."
+                "New Window added to the Tiling Manager " + window.resourceName
             );
             this.logging.printMessage(
                 "Already registered Windows" + this.registeredWindows
@@ -116,24 +116,69 @@ class TilingManager {
     }
 
     removeWindow(window) {
+        this.logging.printMessage(
+            "Window has been removed " + window.resourceName
+        );
+
         const index = this.registeredWindows.indexOf(window);
 
-        if (index > -1) {
+        if (index !== -1) {
             this.registeredWindows.splice(index, 1);
-        }
 
+            this.tileWindowsOnDesktop(
+                this.kwinWrapper.getWindowDesktop(window)
+            );
+        }
+    }
+
+    minimizedChangedForWindow(window) {
+        this.logging.printMessage(
+            "Window minimized stat changed  " + window.resourceName
+        );
+        // execute Tiling for the Desktop, where the Window minimized state changed
+        this.tileWindowsOnDesktop(this.kwinWrapper.getWindowDesktop(window));
+    }
+
+    maximizedChangedForWindow(window) {
+        this.logging.printMessage(
+            "Window maximized stat changed  " + window.resourceName
+        );
+
+        // execute Tiling for the Desktop, where the Window minimized state changed
         this.tileWindowsOnDesktop(this.kwinWrapper.getWindowDesktop(window));
     }
 
     isWindowRelevant(window) {
         // Check wheter the Window is in the List defined in the Configuration to be skipped
-        const ignoreList = ["krunner", "yakuake", "spectacle", "plasmashell"];
+        const ignoreList = [
+            "krunner",
+            "yakuake",
+            "spectacle",
+            "plasmashell",
+            "",
+        ];
         if (ignoreList.indexOf(window.resourceName) !== -1) return false;
 
         // Check if the Window is not relevant based on his basic properties
         if (window.fullScreen) return false;
+        if (window.dialog) return false;
+        if (window.splash) return false;
+        if (window.utility) return false;
+        if (window.dropdownMenu) return false;
+        if (window.tooltip) return false;
+        if (window.notification) return false;
+        if (window.criticalNotification) return false;
+        if (window.appletPopup) return false;
+        if (window.onScreenDisplay) return false;
+        if (window.comboBox) return false;
+        if (window.dndIcon) return false;
+        if (window.specialWindow) return false;
         if (window.minimized) return false;
         if (window.popupWindow) return false;
+        if (window.desktopWindow) return false;
+        if (window.toolbar) return false;
+        if (window.menu) return false;
+
         if (!window.normalWindow) return false;
 
         // #TODO if a Window is set to floated. Not supported yet!
@@ -159,7 +204,11 @@ class TilingManager {
 
         this.registeredWindows.forEach((window) => {
             if (this.kwinWrapper.getWindowDesktop(window) === desktop) {
-                windowsForDesktop.push(window);
+                // Only add the window to the results if it is still in a relevan state
+                // e.g. if change on the minimized, fullscreen, set to floating happens
+                if (this.isWindowRelevant(window)) {
+                    windowsForDesktop.push(window);
+                }
             }
         });
         return windowsForDesktop;
@@ -181,6 +230,14 @@ class Layout {
         // Padding around the Windows in Pixel,
         // it will be considered during the calculation of the Window sizes
         const padding = 10;
+        const rootWindowDefaultSizeInPercentage = 0.65;
+
+        const screenXPos = this.kwinWrapper.getActiveScreenXPos() + padding;
+        const screenYPos = this.kwinWrapper.getActiveScreenYPos() + padding;
+        const screenWidth =
+            this.kwinWrapper.getActiveScreenWidth() - padding * 2;
+        const screenHeight =
+            this.kwinWrapper.getActiveScreenHeight() - padding * 2;
 
         // Define the new position of the windows and set the new geometry
         // First one in the list is always the Root Window, the others follow in the order
@@ -192,12 +249,10 @@ class Layout {
 
         // -> only one window is found, set the maximum size possible
         if (windows.length === 1) {
-            const windowXPos = this.kwinWrapper.getActiveScreenXPos() + padding;
-            const windowYPos = this.kwinWrapper.getActiveScreenYPos() + padding;
-            const windowWidth =
-                this.kwinWrapper.getActiveScreenWidth() - padding * 2;
-            const windowHeight =
-                this.kwinWrapper.getActiveScreenHeight() - padding * 2;
+            const windowXPos = screenXPos;
+            const windowYPos = screenYPos;
+            const windowWidth = screenWidth;
+            const windowHeight = screenHeight;
 
             this.kwinWrapper.setWindowPosition(
                 windows[0],
@@ -207,62 +262,54 @@ class Layout {
                 windowHeight
             );
             this.logging.printMessage(
-                "-> set Window to Fullscreen " + windows[0]
+                "-> set Window to Fullscreen " + windows[0].resourceName
             );
         }
 
         // -> if there are more windows
         if (windows.length > 1) {
-            const rootWindowDefaultSizeInPercentage = 0.65;
             const nrOfSecondaryWindwos = windows.length - 1;
-
-            const screenXPos = this.kwinWrapper.getActiveScreenXPos();
-            const screenYPos = this.kwinWrapper.getActiveScreenYPos();
-            const screenWidth = this.kwinWrapper.getActiveScreenWidth();
-            const screenHeight = this.kwinWrapper.getActiveScreenHeight();
 
             // Determine Root and Secondary Window sizes
             const rootWindowWidth =
                 screenWidth * rootWindowDefaultSizeInPercentage;
-            const rootWindowHeight = screenHeight - padding * 2;
+            const rootWindowHeight = screenHeight;
 
             const secondaryWindowWidth =
-                screenWidth - rootWindowWidth - padding * 3;
-            // e.g. 1000PX screen height with 10PX padding 3 Windows
-            // secondaryWindowHeight = (1000 - 10 * (3+1)) / 3 = 320
+                screenWidth - rootWindowWidth - padding;
             const secondaryWindowHeight =
-                (screenHeight - padding * (nrOfSecondaryWindwos + 1)) /
+                (screenHeight - padding * (nrOfSecondaryWindwos - 1)) /
                 nrOfSecondaryWindwos;
 
             // Set some initial values
-            let windowXPos = screenXPos;
-            let windowYPos = screenYPos;
-            let windowWidth = screenWidth;
-            let windowHeight = screenHeight;
+            let windowXPos = 0;
+            let windowYPos = 0;
+            let windowWidth = 0;
+            let windowHeight = 0;
 
             for (let i = 0; i < windows.length; i++) {
                 // Root Window is always the first one
                 if (i === 0) {
                     windowWidth = rootWindowWidth;
                     windowHeight = rootWindowHeight;
-                    windowXPos = screenWidth - windowWidth - padding;
-                    windowYPos = screenYPos + padding;
+                    windowXPos = screenWidth - windowWidth + padding;
+                    windowYPos = screenYPos;
 
                     this.logging.printMessage(
-                        "-> set Root Window position " + windows[i]
+                        "-> set Root Window position " + windows[i].resourceName
                     );
                 } else {
                     windowWidth = secondaryWindowWidth;
                     windowHeight = secondaryWindowHeight;
-                    windowXPos = screenXPos + padding;
+                    windowXPos = screenXPos;
                     windowYPos =
                         screenYPos +
-                        padding +
                         (i - 1) * secondaryWindowHeight +
                         (i - 1) * padding;
 
                     this.logging.printMessage(
-                        "-> set Secondary Window position " + windows[i]
+                        "-> set Secondary Window position " +
+                            windows[i].resourceName
                     );
                 }
 
@@ -278,8 +325,20 @@ class Layout {
     }
 }
 
-////////////////////////////////////////
-// Initialization and execution
+const Position = { Left: "left", Right: "right" };
+
+class Configuration {
+    constructor() {
+        this.rootWindowDefaultSizeInPercentage = 0.65;
+        this.rootWindowPosition = Position.Right;
+        this.padding = 10;
+        this.ignoreList = ["krunner", "yakuake", "spectacle", "plasmashell"];
+        this.isTilingEnabled = true;
+    }
+}
+
+//---------------------------------------------------------------------------//
+//                      Initialization and execution                         //
 
 // Initialize Wrapper Classes
 const kwinLog = new KWinLog();
@@ -288,15 +347,30 @@ const kwinWrapper = new KWinWrapper();
 // Tiling Manager
 const tilingManager = new TilingManager(kwinLog, kwinWrapper);
 
-var clients = workspace.stackingOrder;
-for (var i = 0; i < clients.length; i++) {
-    tilingManager.registerWindow(clients[i]);
+let windows = workspace.stackingOrder;
+for (let i = 0; i < windows.length; i++) {
+    tilingManager.registerWindow(windows[i]);
 }
 
-workspace.windowAdded.connect(function (client) {
-    tilingManager.registerWindow(client);
+//---------------------------------------------------------------------------//
+//                      Register Hooks                                       //
+
+workspace.windowAdded.connect((window) => {
+    // In case the Window is minimized, we have to react
+    window.minimizedChanged.connect(() => {
+        tilingManager.minimizedChangedForWindow(window);
+    });
+
+    // In case the Window is minimized, we have to react
+    window.maximizedChanged.connect(() => {
+        tilingManager.maximizedChangedForWindow(window);
+    });
+
+    // Attach the Tiling manager for Windows Added
+    tilingManager.registerWindow(window);
 });
 
-workspace.windowRemoved.connect(function (client) {
-    tilingManager.removeWindow(client);
+workspace.windowRemoved.connect((window) => {
+    // Attach the Tiling manager for Windows Removed
+    tilingManager.removeWindow(window);
 });
